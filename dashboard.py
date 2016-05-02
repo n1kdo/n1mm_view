33b0383dcac7729704675262fb3b031e2a103d0f
Jeff Otterson <otterson@gmail.com>
2016-05-01 21:29:16 -0400

    ouch.  change case of fontname

--- dashboard.py
+++ dashboard.py
@@ -43,7 +43,7 @@ GRAY = pygame.Color('#cccccc')
 
 # Initialize font support
 pygame.font.init()
-view_font = pygame.font.Font('veraMoBd.ttf', 64)
+view_font = pygame.font.Font('VeraMoBd.ttf', 64)
 bigger_font = pygame.font.SysFont('veraMoBd.ttf', 180)
 view_font_height = view_font.get_height()
 
