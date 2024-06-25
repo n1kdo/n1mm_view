13e2a7ac3c514992a4b86b372f92a2635bde9e29
n1kdo <otterson@gmail.com>
2024-06-24 20:28:48 -0400

    correct date

--- config.py
+++ config.py
@@ -13,12 +13,12 @@ EVENT_NAME = 'Field Day'
 # EVENT_START_TIME = datetime.datetime.strptime('2019-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
 # EVENT_START_TIME = datetime.datetime.strptime('2021-06-26 18:00:00', '%Y-%m-%d %H:%M:%S')
 # EVENT_START_TIME = datetime.datetime.strptime('2022-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
-EVENT_START_TIME = datetime.datetime.strptime('2024-06-23 18:00:00', '%Y-%m-%d %H:%M:%S')
+EVENT_START_TIME = datetime.datetime.strptime('2024-06-22 18:00:00', '%Y-%m-%d %H:%M:%S')
 """ end time of the event/contest """
 # EVENT_END_TIME = datetime.datetime.strptime('2019-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
 # EVENT_END_TIME = datetime.datetime.strptime('2021-06-27 17:59:59', '%Y-%m-%d %H:%M:%S')
 # EVENT_END_TIME = datetime.datetime.strptime('2022-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
-EVENT_END_TIME = datetime.datetime.strptime('2024-06-24 17:59:59', '%Y-%m-%d %H:%M:%S')
+EVENT_END_TIME = datetime.datetime.strptime('2024-06-23 17:59:59', '%Y-%m-%d %H:%M:%S')
 """ port number used by N1MM+ for UDP broadcasts This matches the port you set in N1MM Configurator UDP logging """
 N1MM_BROADCAST_PORT = 12060
 """ 
