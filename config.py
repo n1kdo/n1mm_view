e4e4222a13f3dd560309be2500630c6c67b9326d
n1kdo <otterson@gmail.com>
2024-06-30 12:54:52 -0400

    update config data

--- config.py
+++ config.py
@@ -8,16 +8,11 @@ import logging
 """ name of database file """
 DATABASE_FILENAME = 'n1mm_view.db'
 """ Name of the event/contest """
-EVENT_NAME = 'Field Day'
+EVENT_NAME = 'N4N Field Day'
 """ start time of the event/contest in YYYY-MM-DD hh:mm:ss format """
-# EVENT_START_TIME = datetime.datetime.strptime('2019-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
-# EVENT_START_TIME = datetime.datetime.strptime('2021-06-26 18:00:00', '%Y-%m-%d %H:%M:%S')
-# EVENT_START_TIME = datetime.datetime.strptime('2022-06-25 18:00:00', '%Y-%m-%d %H:%M:%S')
+
 EVENT_START_TIME = datetime.datetime.strptime('2024-06-22 18:00:00', '%Y-%m-%d %H:%M:%S')
 """ end time of the event/contest """
-# EVENT_END_TIME = datetime.datetime.strptime('2019-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
-# EVENT_END_TIME = datetime.datetime.strptime('2021-06-27 17:59:59', '%Y-%m-%d %H:%M:%S')
-# EVENT_END_TIME = datetime.datetime.strptime('2022-06-26 17:59:59', '%Y-%m-%d %H:%M:%S')
 EVENT_END_TIME = datetime.datetime.strptime('2024-06-23 17:59:59', '%Y-%m-%d %H:%M:%S')
 """ port number used by N1MM+ for UDP broadcasts This matches the port you set in N1MM Configurator UDP logging """
 N1MM_BROADCAST_PORT = 12060
@@ -27,8 +22,7 @@ This could be the IP of the N1MM master, or just the last address in the network
 """
 N1MM_BROADCAST_ADDRESS = '192.168.1.255'
 """ n1mm+ log file name used by replayer """
-# N1MM_LOG_FILE_NAME = 'MyClubCall-2019.s3db'
-N1MM_LOG_FILE_NAME = 'fd2024.s3db'
+N1MM_LOG_FILE_NAME = 'FD2024-N4N.s3db'
 """ QTH here is the location of your event. We mark this location with a red dot when we generate the map views."""
 """ QTH Latitude """
 QTH_LATITUDE = 34.0109629
@@ -42,12 +36,11 @@ Too high makes a lag in viewing your results.
 """
 DATA_DWELL_TIME = 60
 """ log level for apps -- one of logging.WARN, logging.INFO, logging.DEBUG """
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
 #
 """images directory, or None if not writing image files"""
-IMAGE_DIR = '/mnt/ramdisk/n1mm_view/html'
-IMAGE_WIDTH = 1920
-IMAGE_HEIGHT = 1080
+IMAGE_DIR = None  #  '/mnt/ramdisk/n1mm_view/html'
+
 """ set HEADLESS True to not open graphics window. This is for using only the Apache option."""
 HEADLESS = False
 
